0ee27b5a976c78fcfbe8d2edec533926aff4ee83
NicoZeiss <zeissern@gmail.com>
2024-05-27 00:32:17 +0200

    Add Docker support and reorganize file structure
    This commit includes the addition of a Dockerfile and docker-compose.yml to introduce Docker support. It also includes the alteration of the SECRET_KEY in settings.py for added security. Additionally, several files within the 'hello_django' app have been restructured for better organization and maintainability.

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f%zz$j1ie*fuldxcg=n9)*cn2#o(_)nx*64p94epum*nc&(k16'
+SECRET_KEY = 'django-insecure-1@gt+12!g**b1be06x02ucoi6r_v3pq(*wg-r817$m=n12qpuc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
