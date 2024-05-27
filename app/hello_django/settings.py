f00b83a381d337f87489eef6fab909b98ca02d9b
NicoZeiss <zeissern@gmail.com>
2024-05-28 01:56:52 +0200

    Establish 'upload' Django app, upgrade Docker configuration
    This commit includes the establishment of the 'upload' Django app with a primary focus on enhancing Docker configuration for both development and production. It also encompasses the removal of 'entrypoint.sh', improvement of Django project organization, addition of 'gunicorn' and

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
